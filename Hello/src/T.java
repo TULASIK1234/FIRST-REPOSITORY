4f1c570c2a7adebd0a733b279d1de5d20b0a3bb1
TULASIK1234 <koganti.tulasi05@gmail.com>
2025-01-23 23:54:10 -0600

    Update T.java

--- Hello/src/T.java
+++ Hello/src/T.java
@@ -3,8 +3,7 @@ public class T {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//HelloWorld Program
-System.out.println("HelloWorld);
+System.out.println();
 	}
 
 }
